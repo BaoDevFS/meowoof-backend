f0611c90017c69b94508bbd8dbbb37551ab173fe
BaoDev <nguyennhubao999@gmail.com>
2021-06-03 18:10:59 +0700

    edit auth.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
 const functions = require("firebase-functions")
 const admin = require("firebase-admin")
+const {
+    GraphQLClient,
+    gql
+} = require('graphql-request')
+
 admin.initializeApp(functions.config().firebase)
 
 exports.processSignUp = functions.region("asia-east2").auth.user().onCreate((user) => {
+    const graphQLClient = new GraphQLClient("http://203.113.148.132:22030/v1/graphql", {
+        headers: {
+            'content-type': 'application/graphql',
+            'x-hasura-admin-secret': 'meowoof!'
+        },
+    })
     const customUserClaims = {
         'https://hasura.io/jwt/claims': {
             'x-hasura-default-role': 'user',
@@ -10,18 +21,31 @@ exports.processSignUp = functions.region("asia-east2").auth.user().onCreate((use
             'x-hasura-user-id': user.uid, // user.uid,
         },
     }
+    const mutation = gql `mutation MyMutation( $email: String! , $name: String! , $phone_number: String! , $uid: String! , $url: String! ) {
+        insert_users_one(object: {email: $email, phone_number: $phone_number, uid: $uid, avatar_current: {data: {url: $url}}, name: $name}) {
+          id
+        }
+      }
+      
+      `
+    const variables = {
+        email: user.email,
+        name: user.displayName,
+        phone_number: user.phoneNumber,
+        uid: user.uid,
+        url: user.photoURL
+    }
 
     return (
         admin
         .auth()
         .setCustomUserClaims(user.uid, customUserClaims)
-        // .then(() => {
-        //   // Update real-time database to notify client to force refresh.
-        //   const metadataRef = admin.database().ref('metadata/' + user.uid)
-        //   // Set the refresh time to the current UTC timestamp.
-        //   // This will be captured on the client to force a token refresh.
-        //   return metadataRef.set({ refreshTime: new Date().getTime() })
-        // })
+        .then(() => {
+            graphQLClient.request(mutation, variables).then((data) => {
+                console.log(data)
+            });
+
+        })
         .catch((error) => {
             console.log(error)
         })
